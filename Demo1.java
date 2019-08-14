d22ab1ba5858ad10347c2a6ff5ca3eda08660a6a
Bhagyashree005 <bgkatapur@gmail.com>
2019-08-14 11:51:53 +0530

    first commit

---
+++ Demo1.java
@@ -0,0 +1,18 @@
+package Simpleprograms;
+
+public class Demo1 {
+int id;
+String name;
+int DOJ;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Demo1 d = new Demo1();
+d.name = "Siri";
+d.id = 123;
+System.out.println("Hello" + d.id);
+
+
+	}
+
+}
